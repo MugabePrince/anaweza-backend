bbb284d55138489d8ecee29c15229d016334cf82
MugabePrince <princemugabe567@gmail.com>
2025-02-26 23:26:05 +0200

    added user names

--- job_seeker/serializers.py
+++ job_seeker/serializers.py
@@ -5,7 +5,7 @@ from userApp.models import CustomUser  # Import your custom user model
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'phone_number', 'email', 'role', 'status', 'created_at']
+        fields = ['id', 'phone_number', 'email', 'role', 'status',  'created_at', 'profile_picture']
 
 class JobSeekerSerializer(serializers.ModelSerializer):
     user = CustomUserSerializer(read_only=True)
