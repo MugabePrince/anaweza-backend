bef9a52bffccf052c604c621e38e65deb78ff2be
MugabePrince <princemugabe568@gmail.com>
2025-03-08 12:06:06 +0200

    added user profile management

--- userApp/models.py
+++ userApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.timezone import now
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number, role, email=None, password=None, status=False, profile_picture=None):
+    def create_user(self, phone_number, role, email=None, password=None, status=True, profile_picture=None):
         if not phone_number:
             raise ValueError("The phone number must be provided")
         if not role:
